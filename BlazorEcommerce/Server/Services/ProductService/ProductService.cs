1ab6706feddc1152505048a90eac02054b1a8d20
petemennetti <peter.mennetti@gmail.com>
2024-06-30 11:45:00 -0400

    Added Select control

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -12,8 +12,11 @@ namespace BlazorEcommerce.Server.Services.ProductService
 
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
-            var response = new  ServiceResponse<Product>(); 
-            var product = await _context.Products.FindAsync(productId); 
+            var response = new  ServiceResponse<Product>();
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType) 
+                .FirstOrDefaultAsync(p=> p.Id ==productId );
             if (product == null)
             {
                 response.Success = false;
@@ -30,7 +33,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.ToListAsync() 
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync() 
 
             };  
             return response;
@@ -42,6 +45,7 @@ namespace BlazorEcommerce.Server.Services.ProductService
             {
                 Data = await _context.Products
                 .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .Include(p => p.Variants)   
                 .ToListAsync()
             };
             return response;
